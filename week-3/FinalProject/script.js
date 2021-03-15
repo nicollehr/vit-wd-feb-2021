43f0512ba61b87de88a72088288c3ec25476fed8
Nicolle <nicollehr@gmail.com>
2021-03-15 18:09:47 -0500

    FinalProject

---
+++ week-3/FinalProject/script.js
@@ -0,0 +1,8 @@
+
+
+const place = ["Pizzeria", "Beach", "Path of Gods", "Sirenuses", "Monterosso", "Le Bonta del Capo"];
+
+const random = Math.floor(Math.random() * place.length);
+console.log(random, place[random]);
+
+
--- week-3/day-4/exercise.js
+++ week-3/day-4/exercise.js
@@ -1,5 +1,6 @@
 function calculateTip() {
-  var billAmountValue = document.getElementById("billAmount").nodeValue;
+  var billAmountValue = document.getElementById("billAmount").value;
+  console.log(billAmountValue);
   var tipAmount = billAmountValue * 0.15;
   var outputElement = document.getElementById("output");
   outputElement.innerText = tipAmount;
